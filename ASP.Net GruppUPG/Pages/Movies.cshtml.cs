8efc8149f752ce79aeeff898d21cb88aaadb1aef
David <davidhu_94@hotmail.com>
2024-04-29 15:29:18 +0200

    razorpage

---
+++ ASP.Net GruppUPG/Pages/Movies.cshtml.cs
@@ -0,0 +1,48 @@
+using ASP.Net_GruppUPG.Objects;
+using ASP.Net_GruppUPG.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+
+namespace ASP.Net_GruppUPG.Pages
+{
+    public class MoviesModel : PageModel
+    {
+        private readonly MovieService _movieService;
+
+        public MoviesModel(MovieService movieService)
+        {
+            _movieService = movieService;
+        }
+
+        public List<Movie> movies { get; set; }
+
+        [BindProperty]
+        public Movie NewMovie { get; set; }
+
+        public void OnGet()
+        {
+            movies = _movieService.GetMovies();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                _movieService.AddMovie(NewMovie);
+                return RedirectToPage();
+            }
+            return Page();
+        }
+
+        public IActionResult OnPostDelete(int movieId)
+        {
+            var result = _movieService.DeleteMovieById(movieId);
+            if (!result)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
